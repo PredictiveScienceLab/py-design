c561e2d25464043f3c7ec3ee15effc8d5ae430f5
Ilias Bilionis <ebilionis@gmail.com>
2014-12-02 21:22:39 -0500

    Fixed bug.

--- setup.py
+++ setup.py
@@ -11,7 +11,5 @@ setup(name='Design points for random experiments',
       author='Ilias Bilionis',
       version='0.0',
       ext_modules=[Extension('design._design',
-                            glob.glob(os.path.join('src', '*.f90')),
-                             libraries=['python2.7', 'pthread'],
-                             extra_link_args=['-shared'])],
+                            glob.glob(os.path.join('src', '*.f90')))],
       packages=['design'])
